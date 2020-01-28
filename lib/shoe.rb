732698f9af306d381204fa45a4b6d5131de165a4
Kyle McKenna <mckennakyler@gmail.com>
2020-01-28 23:03:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,13 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  BRANDS = [] 
   
   attr_reader :brand
 
   def initialize(brand)
     @brand = brand
   end
+
+  BRANDS = [] 
 
   def cobble
     self.condition = "new"
